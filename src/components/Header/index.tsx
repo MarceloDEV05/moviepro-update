998331ac98413da31cee38a0dd477db3a4d3d6d1
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2025-05-19 16:15:26 -0300

    alteracao no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export const Header = () => {
     return(
-        <header className="w-full h-16 justify-around items-center flex bg-gray-700">
+        <header className="w-full h-16 justify-between px-20 items-center flex bg-gray-700">
             <h1 className="text-3xl text-white">Movie<strong className="text-green-500">PRO</strong></h1>
 
             <ul className="flex gap-10 text-white">
