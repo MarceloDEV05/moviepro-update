aa305417a53bcc265f093b7898ae379b6e839cd4
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2025-05-20 00:07:29 -0300

    alterando comportamento do menu ao clicar fora dele

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom"
-import { useState, useEffect } from "react"
+import { useState, useEffect,useRef } from "react"
 import { FaBars } from "react-icons/fa"
 
 export const Header = () => {
     const [mobile, setMobile] = useState(false)
     const [menu, setMenu] = useState(false)
+    const menuRef = useRef<HTMLDivElement | null>(null)
 
     useEffect(() => {
         const screenMobile = window.matchMedia('(max-width: 640px)');
         setMobile(screenMobile.matches);
 
-        const handler = (e : {matches:boolean}) => setMobile(e.matches)
+        const handler = (e: {matches:boolean}) => setMobile(e.matches)
         screenMobile.addEventListener('change', handler)
 
         return () => screenMobile.removeEventListener('change', handler)
@@ -18,14 +19,25 @@ export const Header = () => {
 
     const openMenu = () => setMenu(open => !open)
     
+    useEffect(() => {
+        const outClick = (e: MouseEvent) => {
+            if(menuRef.current && !menuRef.current.contains(e.target as Node)){
+                setMenu(false)
+            }
+        }
+        document.addEventListener('mousedown', outClick)
+        return() => document.removeEventListener('mousedown', outClick)
+    },[])
+
+
 
     return(
         <header 
         className="w-full h-16 justify-around items-center flex bg-gray-700">
             <h1 className="text-3xl text-white">Movie<strong className="text-green-500">PRO</strong></h1>
                {mobile? (
-                <>
-                  <button onClick={openMenu} className=" cursor-pointer text-white ">
+                <div ref={menuRef}>
+                  <button onClick={openMenu}  className=" cursor-pointer text-white ">
                     <FaBars/>
                   </button>
 
@@ -54,7 +66,7 @@ export const Header = () => {
                     </ul>
                   </div>
                  )}
-                  </>
+                  </div>
                ):(
                 <>
                  <ul className="lg:flex gap-10 text-white sm:max-w-[600px] hidden">
