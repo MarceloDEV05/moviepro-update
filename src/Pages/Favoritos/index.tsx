afaa53ae9b07b95021181deeead3c9872ea944e7
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2025-05-21 01:26:25 -0300

    criando pagina de filmes salvos e adicionando responsividade

--- src/Pages/Favoritos/index.tsx
+++ src/Pages/Favoritos/index.tsx
@@ -20,17 +20,17 @@ export const Favoritos = () => {
                 </>
             )}
 
-            <ul className="mt-5">
+            <ul className="mt-10">
                 {savedMovies.map((movieList) => (
-                    <li key={movieList?.id}>
-                       <section className="flex px-8 py-8 w-full justify-around items-center">
+                    <li key={movieList?.id} className="flex justify-center w-full">
+                       <section className="flex-col lg:flex px-8 py-8 w-full justify-center lg:justify-around items-center">
                         <article className="">
-                            <img src={`https://image.tmdb.org/t/p/original/${movieList.backdrop_path}`} alt="" className="w-80 rounded"/>
+                            <img src={`https://image.tmdb.org/t/p/original/${movieList.backdrop_path}`} alt="" className="w-full lg:w-80 rounded"/>
                         </article>
 
                         <div>
                             <h1 className="font-medium text-2xl">{movieList.title}</h1>
-                            <p className="text-gray-700 line-clamp-3 overflow-hidden w-[600px] italic">{movieList.overview}</p>
+                            <p className="text-gray-700 line-clamp-3 overflow-hidden w-[400px] lg:w-[600px]  italic">{movieList.overview}</p>
                         </div>
 
                         <button className="bg-gray-800 text-white p-2 rounded w-20">excluir</button>
