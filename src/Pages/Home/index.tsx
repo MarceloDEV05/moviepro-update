7c5f7795680150428b49f40ae1821e79dfe4fe55
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2025-05-21 00:20:08 -0300

    criando pagina de detalhes das series

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -9,6 +9,7 @@ export interface FilmesProps {
     poster_path: string;
     backdrop_path: string;
     overview: string;
+    name:string;
 }
 
 export const Home = () => {
@@ -57,7 +58,7 @@ export const Home = () => {
                         key={filme.id}
                     >
         
-                        <article className="group flex flex-col bg-white rounded-lg overflow-hidden transition-transform w-full max-w-xs lg:h-[500px] sm:[400px] hover:scale-105 "
+                        <article className="group flex flex-col bg-white rounded-lg overflow-hidden transition-transform w-full max-w-xs lg:h-[500px] h-400px hover:scale-105 "
                         >
                             <img
                                 src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
@@ -93,14 +94,14 @@ export const Home = () => {
     <h1 className="mt-10 ml-10 font-medium text-3xl">
                 Filmes em Alta
             </h1>
-            <main className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full min-h-screen gap-3 p-2">
+              <main className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full  lg:min-h-screen gap-3 p-2">
                 {filmesAlta.map((emAlta) => (
                     <Link to={`/detalhesfilme/${emAlta.id}`}>
                     <section className="w-full pt-5 flex items-center justify-center"
                         key={emAlta.id}
                     >
-
-                        <article className="group flex flex-col bg-white rounded-lg overflow-hidden transition-transform w-full max-w-xs h-[400px] lg:h-[500px] hover:scale-105 relative"
+        
+                        <article className="group flex flex-col bg-white rounded-lg overflow-hidden transition-transform w-full max-w-xs lg:h-[500px] h-400px hover:scale-105 "
                         >
                             <img
                                 src={`https://image.tmdb.org/t/p/original/${emAlta.poster_path}`}
@@ -108,9 +109,9 @@ export const Home = () => {
                                 className='w-full h-80 lg:h-100 object-cover flex rounded-t-lg transition duration-300 group-hover:brightness-30'
                             />
 
-                            <div className="absolute bottom-35 bg-opacity-60 opacity-0 group-hover:opacity-100 translate-y-8 duration-300 flex flex-col justify-end">
+                            <div className="absolute bottom-20 lg:bottom-35 bg-opacity-60 opacity-0 group-hover:opacity-100 translate-y-8 duration-300 flex flex-col justify-end">
 
-                                <h1 className="text-lg text-white font-medium px-4 truncate ">
+                                <h1 className="text-lg text-white font-medium px-4 ">
                                     {emAlta.title}
                                 </h1>
                                 <p className="text-gray-300 line-clamp-4 overflow-hidden mx-5">
@@ -119,14 +120,17 @@ export const Home = () => {
                                 
                             </div>
 
+                            
                             <button className="cursor-pointer bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded-b-lg transition-colors">
                                 Acessar
                             </button>
+                             
                         </article>
                     </section>
                     </Link>
                 ))}
             </main>
+
 
         </div>
     );
--- src/Pages/SeriesDetails/index.tsx
+++ src/Pages/SeriesDetails/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { type FilmesProps } from "../Home"
 import api from "../../Services/api"
 
@@ -14,7 +14,7 @@ export const SeriesDetails = () => {
                 api_key: "a440320973db39fc00de6bdcb4604c9b",
                 }
             })
-           
+           console.log(response.data)
             setSerieDetail(response.data)
         }
 
@@ -22,17 +22,26 @@ export const SeriesDetails = () => {
     },[id])
     return(
         <div>
-            <main>
+            <main className=" lg:px-50 flex w-full justify-center h-screen">
                 {serieDetail && (
-                    <section key={serieDetail?.id}>
+                    <section key={serieDetail?.id} className="px-10">
                         <article>
-                          <img src={`https://image.tmdb.org/t/p/original/${serieDetail?.backdrop_path}`} alt="" />
+                          <h1 className="text-2xl text-center font-medium mt-10 mb-10">{serieDetail?.name}</h1>
+                          <img src={`https://image.tmdb.org/t/p/original/${serieDetail?.backdrop_path}`} alt="" className="h-80 lg:h-100 w-full rounded-lg object-cover" />
                         </article>
                          
                          <div>
-                            <h1>{serieDetail?.title}</h1>
+                            <h2 className="font-medium text-xl text-gray-800">Sinopse:</h2>
                             <p>{serieDetail?.overview}</p>
-                            <strong>{serieDetail?.vote_average}</strong>
+                            <strong className="text-green-600 font-bold">Avaliações: {serieDetail?.vote_average.toFixed(1)}/10</strong>
+                         </div>
+
+                         <div className="flex gap-6 mt-5">
+                            <button className="p-2 cursor-pointer bg-green-500 hover:bg-green-700 transition duration-300 w-25 rounded-md text-white">Salvar</button>
+
+                             <a target="_blank" rel="external" href={`https://youtube.com/results?search_query=${serieDetail.name} trailer`}>
+                            <button className="p-2 cursor-pointer bg-gray-800 transition duration-300 hover:bg-gray-950 w-25 rounded-md text-white">Trailer</button>
+                            </a>
                          </div>
                     </section>
                 )}
